4c16ee48f010f227187da435ad5c4219f3cc7f02
Dani Martín Sáiz <danimartinsaiz@gmail.com>
2024-02-01 18:49:36 +0100

    registro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
+import { RegistroComponent } from './registro/registro.component';
 
 
 const routes: Routes = [
    {path: 'inicio', component: InicioComponent},
+   {path: 'registro', component: RegistroComponent},
    {path: '', redirectTo: '/inicio', pathMatch: 'full'}
 ];
 
