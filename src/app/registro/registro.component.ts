3cc741d63a43380a628f029876c81ba97b9c94e9
Dani Martín Sáiz <danimartinsaiz@gmail.com>
2024-02-05 01:43:33 +0100

    registro y login correcto ya se pueden registrar y logear

--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -1,39 +1,60 @@
 // registro.component.ts
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
-import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
-  styleUrls: ['./registro.component.css']
+  styleUrls: ['./registro.component.css'],
 })
 export class RegistroComponent {
-  isLoginForm: boolean = true;
-  private url: string = 'https://localhost:7089/';
+  isLoginForm = true; // Establece esto a false para mostrar el formulario de registro
+  registerDto: any = {}; // Inicializar como objeto vacío
+  loginDto: any = {}; // Inicializar como objeto vacío
+  apiUrl = 'https://localhost:7089/LoginRegister'; // Ajusta la URL base según tu configuración
 
-  //HttpClient
-  constructor(private httpClient: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
   toggleForm() {
     this.isLoginForm = !this.isLoginForm;
   }
 
-  user = {
-    name: '',
-    email: '',
-    address: '',
-    password: '',
-    repeatPassword: '',
-    staySignedIn: false
-  };
-
-  //POST 
-  registerUser(user: any): Observable<any> {
-    return this.httpClient.post(`${this.url + "/registro"}`, user);
+  loginUser(loginDto: any) {
+    // Realiza la solicitud HTTP POST al endpoint de inicio de sesión en tu servidor
+    this.http.post(`${this.apiUrl}/Login`, loginDto)
+      .subscribe(
+        (response) => {
+          console.log('Inicio de sesión exitoso', response);
+        },
+        (error) => {
+          console.error('Error al iniciar sesión', error);
+        }
+      );
   }
 
+  registerUser(registerDto: any) {
+    // Realiza la solicitud HTTP POST al endpoint de registro en tu servidor
+    this.http.post(`${this.apiUrl}/Register`, registerDto)
+      .subscribe(
+        (response) => {
+          console.log('Registro exitoso', response);
+        },
+        (error) => {
+          console.error('Error al registrar usuario', error);
+        }
+      );
+  }
 
+  fetchData() {
+    // Realiza la solicitud HTTP GET para obtener datos del servidor
+    this.http.get(`${this.apiUrl}`)
+      .subscribe(
+        (data) => {
+          console.log('Datos recibidos', data);
+        },
+        (error) => {
+          console.error('Error al obtener datos', error);
+        }
+      );
+  }
 }
-
-
