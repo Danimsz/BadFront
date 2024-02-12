d520cbfb162039dd927038ac1319a59b576e8c4b
Dani Martín Sáiz <danimartinsaiz@gmail.com>
2024-02-12 18:01:54 +0100

    cambio detalles

--- src/app/detallesproducto/detallesproducto.component.ts
+++ src/app/detallesproducto/detallesproducto.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Producto } from '../producto.model';
+import { CatalogoService } from '../catalogo.service';
+
 
 @Component({
   selector: 'app-detallesproducto',
   templateUrl: './detallesproducto.component.html',
   styleUrls: ['./detallesproducto.component.css']
 })
-export class DetallesproductoComponent {
+export class DetallesproductoComponent implements OnInit  {
+  catalogo: Producto[] = [];
+  constructor(private catalogoService: CatalogoService) { }
 
+  ngOnInit(): void {
+    this.obtenerCatalogo();
+  }
+
+  obtenerCatalogo(): void {
+    this.catalogoService.obtenerCatalogo()
+      .subscribe(
+        (data) => {
+          this.catalogo = data;
+        },
+        (error) => {
+          console.error('Error al obtener el catálogo de productos', error);
+        }
+      );
+  }
 }
+
+
+
