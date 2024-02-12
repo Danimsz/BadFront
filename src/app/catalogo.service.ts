4ed454136e0fc1b67630df8fb709d1be15397bc8
Dani Martín Sáiz <danimartinsaiz@gmail.com>
2024-02-12 20:47:03 +0100

    galeria y detalle que sufrimiento ya esta arreglado ty @danie

--- src/app/catalogo.service.ts
+++ src/app/catalogo.service.ts
@@ -1,16 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, lastValueFrom } from 'rxjs';
 import { Producto } from './producto.model'; 
 @Injectable({
   providedIn: 'root'
 })
+
 export class CatalogoService {
   private apiUrl = 'http://localhost:5174/CatologoProducto/Catalogo'; 
 
   constructor(private http: HttpClient) { }
 
-  obtenerCatalogo(): Observable<Producto[]> {
-    return this.http.get<Producto[]>(this.apiUrl);
+   async obtenerCatalogo(): Promise <Producto[]> {
+    
+
+    const peticion$ = this.http.get<Producto[]>(this.apiUrl);
+    const productos: Producto[] = await lastValueFrom(peticion$);
+
+    return productos;
+
+    this.http.get<Producto[]>(this.apiUrl);
   }
 }
