ded25853d19570a6d6839e569d34d8e5070cc0fc
AmandaCode1 <amandacode1337@gmail.com>
2024-03-03 20:58:34 +0100

    Para probar si aparecen los pedidos del usuario

--- src/app/usuario.service.ts
+++ src/app/usuario.service.ts
@@ -1,19 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { Usuario } from './usuario.model';
+import { Pedido } from './usuario.model';
 
 @Injectable({
   providedIn: 'root'
 })
 
   export class UsuarioService {
-    private apiUrl = 'http://localhost:5174/Usuario'; 
+    private apiUrl = 'http://localhost:5174'; 
   
     constructor(private http: HttpClient) {}
   
     obtenerDatosUsuario(userId: number): Observable<Usuario> {
-      return this.http.get<Usuario>(`${this.apiUrl}/VerUsuario/${userId}`);
+      return this.http.get<Usuario>(`${this.apiUrl}/Usuario/VerUsuario/${userId}`);
     }
+
+    obtenerPedidosUsuario(userId: number): Observable<Pedido[]> {
+      return this.http.get<Pedido[]>(`${this.apiUrl}/Pedido/VerPedidos`).pipe(
+        map(pedidos => pedidos.filter(pedido => pedido.clienteID === userId))
+      );
+    }
+
+
     
   }
